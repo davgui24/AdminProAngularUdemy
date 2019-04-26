e4bfd1718dcb36d28cece2216ce3c97217747db4
David Pereira <davgui242011@gmail.com>
2019-04-26 15:05:13 -0500

    Se obtuvo el token de la autenticacion de google SignIn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,9 +3,10 @@ import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { UsuarioService } from '../services/usuario/usuario.service';
 import { Usuario } from '../models/usuario.model';
-import { Stream } from 'stream';
+
 
 declare function int_plugins();
+declare const gapi: any;
 
 @Component({
   selector: "app-login",
@@ -15,17 +16,55 @@ declare function int_plugins();
 export class LoginComponent implements OnInit {
 
   email: string;
-
   recuerdame: boolean = false;
+
+  auth2: any;
+
   constructor(private router: Router, public usuarioService: UsuarioService) {}
 
   ngOnInit() {
+    this.googleInit();
     int_plugins();
     this.email = localStorage.getItem('email') || '';
     if(this.email.length > 1){
       this.recuerdame = true;
     }
   }
+
+
+
+  // -------------------------
+
+
+
+  googleInit(){
+    gapi.load('auth2', () => {
+       this.auth2 = gapi.auth2.init({
+        client_id: '1021113510695-d7akg1d8qm0jimn44q5qsndrqejv28r3.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+    });
+
+    this.attachSignIn(document.getElementById('btnGoogle'));
+    });
+  }
+
+
+  // -------
+
+  attachSignIn(element){
+   this.auth2.attachClickHandler(element, {}, (googleUser) => {
+
+    //  let profile = googleUser.getBasicProfile();
+     let token  = googleUser.getAuthResponse().id_token;
+
+     console.log(token);
+   });
+  }
+
+
+
+  // ---------------
 
   ingresar(forma: NgForm){
 
